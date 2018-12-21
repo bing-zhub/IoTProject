d06561c862fb25cedcedee33a866e4f749b35ea3
bing.zhub <bing.zhub@gmail.com>
2018-12-21 23:28:53 +0800

    init commit

---
+++ client/client.ino
@@ -0,0 +1,49 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h> 
+
+const char* host = "192.168.4.1";
+int heartbeat = 0;
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin("ESP_D54736");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  Serial.print("connecting to ");
+  Serial.println(host);
+  WiFiClient client;
+  const int httpPort = 80;
+  if (!client.connect("192.168.4.1", httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+  String PostData = "heartbeat="+String(heartbeat++);    
+  client.println("POST /Led HTTP/1.1");
+  client.println("Host: 192.168.4.1");
+  client.println("Cache-Control: no-cache");
+  client.println("Content-Type: application/x-www-form-urlencoded");
+  client.print("Content-Length: ");
+  client.println(PostData.length());
+  client.println();
+  client.println(PostData);        
+  delay(10);
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+  delay(100);
+  Serial.println();
+  Serial.println("closing connection");             
+}
---
+++ server/server.ino
@@ -0,0 +1,42 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+
+const char* ssid = "ESP_D54736"; 
+bool toggle=0;
+ESP8266WebServer server(80);
+
+void handleRoot() {
+  String message = "";
+  
+  if (server.arg("heartbeat")== ""){     //Parameter not found
+  
+  message = "heartbeat Argument not found";
+  
+  }else{     //Parameter found
+  
+  message = "heartbeat  = ";
+  message += server.arg("heartbeat");     //Gets the value of the query parameter
+  
+  }
+  Serial.println(message);
+  server.send(200, "text/plain", message);       //Response to the HTTP request
+}
+
+void setup() {
+  delay(200);
+  Serial.begin(115200);
+  pinMode(2, OUTPUT);
+  WiFi.softAP(ssid);
+
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
+  server.on("/Led", handleRoot);
+  server.begin();
+  Serial.println("Server started");
+}
+
+void loop() {
+  server.handleClient();
+}
