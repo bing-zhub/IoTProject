4bb14681d353f92c560efbca98fcc9b74622134f
bing.zhub <bing.zhub@gmail.com>
2018-12-24 12:18:23 +0800

    works well on transfer data via Nb-iot

--- server/server.ino
+++ server/server.ino
@@ -1,32 +1,35 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
+#include "SoftwareSerial.h"
 
 const char* ssid = "ESP_D54736"; 
-bool toggle=0;
 ESP8266WebServer server(80);
+SoftwareSerial dSerial(D0, D1, false, 256);
 
 void handleData() {
   String message = "";
-  
+  char data[255];
   if (server.arg("heartbeat")== ""){     //Parameter not found
   
-  message = "heartbeat Argument not found";
+    message = "heartbeat Argument not found";
   
   }else{     //Parameter found
   
-  message = "heartbeat  = ";
-  message += server.arg("heartbeat");     //Gets the value of the query parameter
-  
+    message = "heartbeat  = ";
+    message += server.arg("heartbeat");     //Gets the value of the query parameter
+    strcpy(data, message.c_str());
+    dSerial.write(data);
+    yield();
+    Serial.println(data);
   }
-  Serial.println(message);
   server.send(200, "text/plain", message);       //Response to the HTTP request
 }
 
 void setup() {
   delay(200);
-  Serial.begin(115200);
-  pinMode(2, OUTPUT);
+  Serial.begin(9600);
+  dSerial.begin(9600);
   WiFi.softAP(ssid);
 
   IPAddress myIP = WiFi.softAPIP();
@@ -38,5 +41,5 @@ void setup() {
 }
 
 void loop() {
-  server.handleClient();
+  server.handleClient();  
 }
